47fc3fca23f68a19339713f8259dae1cdd7930ff
Aurélien Provenchère <provenchere.aurelien@gmail.com>
2019-04-15 16:27:36 +0200

    - Code Review

--- src/scripts/game/GridManager_class.js
+++ src/scripts/game/GridManager_class.js
@@ -33,30 +33,49 @@ class GameManager {
 
     goRight(){
         let gridIndex = this.player.gridIndex
-        console.log("Grid index : "+gridIndex)
         let relativeMaxPosX = this.player.relativPosX
-        let isAbleToContinue = true
+        let isAbleToContinue = true // Permet de quitter la boucle
         while (isAbleToContinue){
-            console.log("relativeMaxPosX : "+relativeMaxPosX)
-            let currentMatrice = this.grids[gridIndex].matrice
-            let nextCaseValue = currentMatrice[this.player.posY][relativeMaxPosX+1]
-            if (nextCaseValue == 1 || nextCaseValue == 2 || nextCaseValue == 3){
-                relativeMaxPosX++
+            let currentMatrice = this.grids[gridIndex].matrice // Récupérer la matrice sur laquelle le perso est
+            let nextCaseValue = currentMatrice[this.player.posY][relativeMaxPosX+1] // Récupère la valeur de la prochaine case
+            if (nextCaseValue == 1 || nextCaseValue == 2 || nextCaseValue == 3 || nextCaseValue == 11){ // SI case avec possibilité de mouvement
+                relativeMaxPosX++ // Augmentation de la position max sur X
+
+                // Si on sort de la grille actuelle
                 if (relativeMaxPosX+1 > 10) {
-                    gridIndex++
-                    console.log("Grid index : "+gridIndex)
-                    relativeMaxPosX = 0
+                    gridIndex++ // On passe à la grille suivante
+                    relativeMaxPosX = 0 // On revient à la première colonne de la grille suivante
+
+                    // Si on a pas assez de grilles en place -> en générer
+                    while (this.grids.length < gridIndex + 3){
+                        this.generateANewGrid()
+                    }
                 }
+
             } else {
                 isAbleToContinue = false
             }
         }
-        this.player.setNewPosX(relativeMaxPosX,gridIndex)
-        console.log("END :")
-        console.log("Grid index : "+gridIndex)
-        console.log("relativeMaxPosX : "+relativeMaxPosX)
-        console.log(this.player)
-        this.actualizeDisplay()
+        this.player.setNewPosX(relativeMaxPosX,gridIndex) // Change la position sur X de l'utilisateur
+        this.actualizeDisplay() // Change la position du gameCridContainer
+    }
+
+    /*
+    Globalement l'inverse de goRight
+    SAUF QUE
+    - pas besoin de générer des nouvelles grilles
+    - Besoin de bloquer l'utilisateur à la colonne 0 de la grid 0
+     */
+    goLeft(){
+
+    }
+
+    goTop(){
+
+    }
+
+    goBottom(){
+
     }
 
     actualizeDisplay(){
@@ -71,7 +90,7 @@ class GameManager {
         this.player = new Player(character)
         this.grids = []
         this.matrices = []
-/*        this.matrices.push([
+        this.matrices.push([
             [0,0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0,0],
@@ -83,7 +102,7 @@ class GameManager {
             [0,0,0,0,0,0,0,1,1,0,1],
             [0,0,0,0,0,0,0,0,1,1,1],
             [0,0,0,0,0,0,0,0,0,0,0]
-        ])*/
+        ])
         this.matrices.push([
             [0,0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,1,1,1,1,0,0,0],
