6b62f9b5fde28a9fb097fa2434d179f4ab6babf7
RomainPct <romain.penchenat@gmail.com>
2019-04-16 10:37:02 +0200

    - Launch game from character selection

--- src/scripts/game/9_launchGame.js
+++ src/scripts/game/9_launchGame.js
@@ -3,6 +3,13 @@
 Launch game
 -----------
 */
+const chooseCharacterSection = element('#chooseYourCharacter')
+const playNowButton = element('#playNow')
+playNowButton.addEventListener('click',function (e) {
+    e.preventDefault()
+    chooseCharacterSection.style.top = "100%"
+})
+
 const gameGridContainer = element('#gameGridContainer')
 const character = element('.character')
 const gameManager = new GameManager(gameGridContainer,character)
