9c2df799c99cb3bae5a3b408baaee4935e8b17b9
Margot Rasamy <rasamymargot@gmail.com>
2019-04-17 16:12:48 +0200

    1st map modified with wall updates

--- src/scripts/game/0_matrices.js
+++ src/scripts/game/0_matrices.js
@@ -3,12 +3,7 @@
 1 => Case chemin libre
 2 => Case chemin pièce
 3 => Case chemin étoile
-4 => Case mur classique horizontal droit
-5 => Case mur classique horizontal gauche
-6 => Case mur classique vertical haut
-7 => Case mur classique vertical bas
-8 => Case mur angle bas gauche
-9 => Case mur angle bas droite
+4 => Case mur
 10 => Case mur angle haut gauche
 11 => Case mur angle haut droite
 12 => Case mur central
@@ -45,17 +40,17 @@ const initMatrices = [
 ]
 const matrices = [
     [
-        [0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,14,0,0,0],
-        [0,0,0,0,0,2,2,2,0,0,0],
-        [0,0,14,2,1,3,12,3,2,2,0],
-        [0,0,2,1,12,12,12,12,1,2,0],
+        [0,0,0,0,0,4,4,4,4,0,0],
+        [0,0,0,0,4,4,1,14,4,0,0],
+        [0,4,4,4,4,2,2,2,4,4,4],
+        [0,4,14,2,1,3,12,3,2,2,4],
+        [4,4,2,1,12,12,12,12,1,2,4],
         [1,2,1,2,12,14,2,2,2,3,1],
-        [0,0,0,2,1,1,2,0,0,0,0],
-        [0,3,13,3,2,2,3,0,3,0,0],
-        [0,0,0,0,0,0,13,0,3,0,0],
-        [0,0,0,0,0,0,3,3,3,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0]
+        [4,4,4,2,1,1,2,4,4,4,4],
+        [4,3,13,3,2,2,3,4,3,4,0],
+        [4,4,4,4,4,4,13,4,3,4,0],
+        [0,0,0,0,0,4,3,3,3,4,0],
+        [0,0,0,0,0,4,4,4,4,4,0]
     ],
     [
         [0,0,0,0,0,0,0,0,0,0,0],
